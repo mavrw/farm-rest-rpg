eb528420026c9ce968115ed6e83ff363c49f0ded
mavrw <62825029+mavrw@users.noreply.github.com>
2025-05-20 17:07:17 -0500

    implement `plotStore`

--- frontend/src/stores/plotStore.ts
+++ frontend/src/stores/plotStore.ts
@@ -1,6 +1,13 @@
-import type { Plot } from "@/types/plot";
 import { defineStore } from "pinia";
 import { ref } from "vue";
+import { 
+    getAllPlots as apiGetAllPlots,
+    getPlot as apiGetPlot,
+    buyPlot as apiBuyPlot,
+    plantPlot as apiPlantPlot,
+    harvestPlot as apiHarvestPlot,
+} from "@/api/plot";
+import type { Plot } from "@/types/plot";
 
 export const usePlotStore = defineStore('plot', () => {
     // State
@@ -8,16 +15,57 @@ export const usePlotStore = defineStore('plot', () => {
     const error = ref<string | null>(null);
 
     // Actions
-    const fetchPlots = async () => {
-        // TODO: fetch plots from backend using API
+    const fetchPlots = async (farmID: number) => {
+        try {
+            const response: Plot[] = await apiGetAllPlots(farmID);
+            plots.value.clear();
+            response.forEach(plot => plots.value.set(plot.id, plot));
+        } catch (err) {
+            if (err instanceof Error) {
+                error.value = err.message;
+            } else {
+                error.value = String(err);
+            }
+        }
+    };
+
+    const buy = async (farmID: number) => {
+        try {
+            const newPlot: Plot = await apiBuyPlot(farmID);
+            plots.value.set(newPlot.id, newPlot);
+        } catch(err) {
+            if (err instanceof Error) {
+                error.value = err.message;
+            } else {
+                error.value = String(err);
+            }
+        }
     };
 
     const plant = async (plotID: number, cropID: number) => {
-        // TODO: plant plot from backend using API
+        try {
+            const updatedPlot: Plot = await apiPlantPlot(plotID, cropID);
+            plots.value.set(updatedPlot.id, updatedPlot);
+        } catch(err) {
+            if (err instanceof Error) {
+                error.value = err.message;
+            } else {
+                error.value = String(err);
+            }
+        }
     };
 
     const harvest = async (plotID: number) => {
-        // TODO: harvest plot from backend using API
+        try {
+            const updatedPlot: Plot = await apiHarvestPlot(plotID);
+            plots.value.set(updatedPlot.id, updatedPlot);
+        } catch(err) {
+            if (err instanceof Error) {
+                error.value = err.message;
+            } else {
+                error.value = String(err);
+            }
+        }
     };
 
     const getPlot = (id: number) => {
@@ -34,6 +82,7 @@ export const usePlotStore = defineStore('plot', () => {
 
     return {
         fetchPlots,
+        buy,
         plant,
         harvest,
         getPlot,
